07b625259771a60f71f61e77f168d9db1ae5458d
Vishmanthi <fernandovishmanthi@yahoo.com>
2019-05-19 09:35:27 +0530

    toolbar

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,6 +34,14 @@ export class BurgerBuilder extends Component {
     this.setState({purchasing:true});
   }
 
+  purchaseCancelHandler=()=>{
+    this.setState({purchasing:false});
+  }
+
+  purchaseContinueHandler=()=>{
+    alert('You continue!');
+  }
+
 updatePurchaseState(ingredients){
   const sum=Object.keys(ingredients)
   .map(igKey=>{
@@ -87,8 +95,11 @@ removeIngredientHandler=(type)=>{
     }
     return (
         <Aux>
-          <Modal show={this.state.purchasing}>
-            <OrderSummary ingredients={this.state.ingredients}/>
+          <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+            <OrderSummary ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+            price={this.state.totalPrice.toFixed(2)}/>
           </Modal>
       <Burger ingredients={this.state.ingredients}/>
       <BuildControls 
